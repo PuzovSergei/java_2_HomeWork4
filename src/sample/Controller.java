7903bb600d97bbd0dbc27b2d4b3e369c95e7006b
PuzovSergei <s_puzov@mail.ru>
2022-02-14 03:09:51 +0500

    ДЗ

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,24 +1,24 @@
-//package sample;
-//
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.scene.control.TextArea;
-//import javafx.scene.control.TextField;
-//
-//public class Controller {
-//    @FXML
-//    public TextField textField;
-//    @FXML
-//    public TextArea textArea;
-//
-//    @FXML
-//    public void btn1Click(ActionEvent actionEvent) {
-//        System.out.println("Hello world!");
-//    }
-//    @FXML
-//    public void sendMsg(ActionEvent actionEvent) {
-//        textArea.appendText(textField.getText()+"\n");
-//        textField.clear();
-//        textField.requestFocus();
-//    }
-//}
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    public TextField textField;
+    @FXML
+    public TextArea textArea;
+
+    @FXML
+    public void btn1Click(ActionEvent actionEvent) {
+        System.out.println("Hello world!");
+    }
+    @FXML
+    public void sendMsg(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText()+"\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+}
